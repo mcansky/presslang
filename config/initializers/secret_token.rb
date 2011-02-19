548c7cf9c2d13159c43881379cb1e4b264aaf6b2
mcansky <mcansky@arbousier.info>
2011-02-19 16:49:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presslang::Application.config.secret_token = '896a27d293bc223084b42222a3db8716f14573581709c1b469ef88b34aa1e084ef7626e87b7c942366f1939675a6b283eeebda3cfa8559ffd5dd0572f6823aec'
